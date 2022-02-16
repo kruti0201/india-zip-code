0dbe4eeb0c1af6620d6b8f7872d5e95387833057
Kruti Shah <krutis0201@gmail.com>
2022-02-17 00:07:17 +0530

    india zip code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {data} from './data'
+import React, { Ref } from 'react';
 
-function App() {
+export default function App() {
+  React.useEffect(() => {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    data.map((countryData)=>
+    {
+      // const tozero = eval(countryData.key) - 100000
+      // const inToRange = tozero/750000
+      // const multipleOf = inToRange * 768 - 50
+      // ctx.fillStyle = 'rgb('+multipleOf>256? 255 : multipleOf+', '+multipleOf>256? multipleOf>512? 255 : multipleOf - 256 : 0+', '+multipleOf>256? multipleOf>512? multipleOf - 512 : 0 : 0+')'
+      // console.log(multipleOf)
+      ctx.fillStyle = "#" + ((eval(countryData.key)-100000)*50).toString(16);
+      ctx.fillRect((countryData.latitude)*20 , (countryData.longitude)*20-1350, 5, 5);
+    }
+    )
+   
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas
+        id="myCanvas"       
+        width="1000"
+        height="600"
+      >
+        Your browser does not support the HTML canvas tag.
+      </canvas>
     </div>
   );
 }
-
-export default App;
